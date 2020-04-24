9d65ad6e3bbdc823b5d2979f099087236e2f3880
mfankhauser <57176519+mfankhauser@users.noreply.github.com>
2020-04-24 18:41:51 +0200

    Add files via upload
    Ich bin noch nicht fertig mit meiner Arbeit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WortKette.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Wortkette beinhaltet die gesamten Wörter
+            // Ich bin bei Beiden noch nicht fertig
+            string wortkette = "";
+
+
+            string eingabe = "-";
+            while (eingabe != "")
+            {
+            
+
+            }
+            if (eingabe.CompareTo(eingabe.EndsWith))
+            {
+                wortkette.Add;
+            }
+            else
+            {
+                Console.WriteLine("Das ist falsch! Das sind Ihre erziehlten Wörter.", wortkette);
+
+            }
+            
+
+
+            Console.WriteLine("Wortkette: {0}", wortkette);
+        }
+    }
+}
